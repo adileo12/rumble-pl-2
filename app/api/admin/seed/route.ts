4b65ee050b7947e903b5444b1e02009e551f9c20
adileo12 <84244261+adileo12@users.noreply.github.com>
2025-08-09 09:05:34 -0400

    Update route.ts

--- app/api/admin/seed/route.ts
+++ app/api/admin/seed/route.ts
@@ -1,5 +1,7 @@
 import { NextResponse } from 'next/server';
-import { db } from '@/src/lib/db';
+- import { db } from '@/src/lib/db';
++ import { db } from '../../../../src/lib/db';
+
 
 export async function POST() {
   try {
