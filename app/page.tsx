060af927ec8ac2f403ae06b80736d51d8bba102e
adileo12 <84244261+adileo12@users.noreply.github.com>
2025-08-12 15:48:15 -0400

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,11 @@
+export const dynamic = 'force-dynamic';
+export const revalidate = 0;
+
+import { cookies } from "next/headers";
 import { redirect } from "next/navigation";
-export default function Page() {
-  redirect("/login");
+
+export default function Root() {
+  const sid = cookies().get("sid")?.value;
+  if (sid) return redirect("/home");
+  return redirect("/login");
 }
