06061d1f51eda94bc5720e246cc657c5269fd3d3
adileo12 <84244261+adileo12@users.noreply.github.com>
2025-08-12 22:15:06 -0400

    Update route.ts

--- app/api/auth/admin-login/route.ts
+++ app/api/auth/admin-login/route.ts
@@ -1,6 +1,6 @@
 import { NextResponse } from "next/server";
 import bcrypt from "bcryptjs";
-import db from "@/lib/db";
+import { db } from "@/src/lib/db";
 
 export async function POST(req: Request) {
   try {
