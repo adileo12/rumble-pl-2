1bd75b58c9d85ee31c6e8ea2c8be5fe025e5ea96
adileo12 <84244261+adileo12@users.noreply.github.com>
2025-08-20 22:11:53 -0400

    Update layout.tsx

--- app/(protected)/rumble/layout.tsx
+++ app/(protected)/rumble/layout.tsx
@@ -44,3 +44,17 @@ export default async function RumbleLayout({ children }: { children: React.React
     </div>
   )
 }
+
+
+export default function RumbleLayout({ children }: { children: React.ReactNode }) {
+  return (
+    <div className="max-w-6xl mx-auto p-6 space-y-6">
+      <nav className="flex gap-4 text-sm">
+        <Link href="/rumble/play" className="underline">Play</Link>
+        <Link href="/rumble/leaderboard" className="underline">Leaderboard</Link>
+        <Link href="/rumble/rules" className="underline">Rules</Link> {/* NEW */}
+      </nav>
+      {children}
+    </div>
+  );
+}
