34e5fe27dd2cd71c0fe5c28095600f51393ba104
adileo12 <84244261+adileo12@users.noreply.github.com>
2025-08-21 22:15:26 -0400

    Update layout.tsx

--- app/(admin-only)/admin/layout.tsx
+++ app/(admin-only)/admin/layout.tsx
@@ -1,10 +1,11 @@
 "use client";
 
+import React from "react";
 import Link from "next/link";
 import { useSearchParams } from "next/navigation";
 
 export const dynamic = "force-dynamic";
-export const revalidate = false;
+export const revalidate = false as const;
 export const runtime = "nodejs";
 
 const TABS = [
