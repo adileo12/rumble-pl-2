16ae8317ac05221b001bded2764cc1b5e143a53e
adileo12 <84244261+adileo12@users.noreply.github.com>
2025-08-21 22:52:18 -0400

    Delete app/(admin-only) directory

--- app/(admin-only)/layout.tsx
+++
@@ -1,7 +0,0 @@
-export const dynamic = "force-dynamic";
-export const revalidate = false;
-export const runtime = "nodejs";
-
-export default function GroupLayout({ children }: { children: React.ReactNode }) {
-  return <>{children}</>;
-}
