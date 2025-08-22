60fdc2d055367047e60db83aebb51e6b3e50efdc
adileo12 <84244261+adileo12@users.noreply.github.com>
2025-08-21 22:08:13 -0400

    Create layout.tsx

---
+++ app/(admin-only)/layout.tsx
@@ -0,0 +1,7 @@
+export const dynamic = "force-dynamic";
+export const revalidate = false;
+export const runtime = "nodejs";
+
+export default function GroupLayout({ children }: { children: React.ReactNode }) {
+  return <>{children}</>;
+}
