55ddc4e44b1f07f50649e36b46ab1c60b84814cf
adileo12 <84244261+adileo12@users.noreply.github.com>
2025-08-24 20:35:07 -0400

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,13 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   webpack: (config) => {
-    // Needed so Next can bundle & run the WASM for resvg on the server
-    config.experiments = {
-      ...(config.experiments || {}),
-      asyncWebAssembly: true,
-    };
+    config.experiments = { ...(config.experiments || {}), asyncWebAssembly: true };
     return config;
   },
 };
-
 module.exports = nextConfig;
