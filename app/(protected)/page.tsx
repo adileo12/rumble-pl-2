4c188282bbae83e36bc4b6ab00ea390a56414c37
adileo12 <84244261+adileo12@users.noreply.github.com>
2025-08-25 07:24:36 -0400

    Delete app/(protected)/page.tsx

--- app/(protected)/page.tsx
+++
@@ -1,6 +0,0 @@
-import { redirect } from "next/navigation";
-
-export const dynamic = "force-dynamic";
-export default function ProtectedRoot() {
-  redirect("/home"); // send logged-in users to our new home page
-}
